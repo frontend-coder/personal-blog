64dcaa3c58b966149a2de16c356209596a0e51e8
frontendercode <frontendercode@gmail.com>
2021-01-20 15:41:29 +0200

    v.0.26
    Настроена работа логики на всех страницах шаблона

--- gulpfile.js
+++ gulpfile.js
@@ -66,10 +66,10 @@ function browsersync() {
 
 function scripts() {
   return src([
-    'app/libs/plagins/jquery.min.js',
-    'app/libs/plagins/page-scroll-to-id-master/js/minified/jquery.malihu.PageScroll2id.min.js',
-    'app/libs/plagins/magnific-popup/jquery.magnific-popup.min.js',
-    'app/libs/plagins/slick/slick.min.js',
+ 'app/libs/plagins/jquery.min.js',
+    // 'app/libs/plagins/page-scroll-to-id-master/js/minified/jquery.malihu.PageScroll2id.min.js',
+    // 'app/libs/plagins/magnific-popup/jquery.magnific-popup.min.js',
+    // 'app/libs/plagins/slick/slick.min.js',
     'app/libs/common.js',
   ])
     .pipe(strip())
